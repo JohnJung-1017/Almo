10f144e3724fde470257aadef03a6e464ffa4473
SooooCoding <poomisujin05@gmail.com>
2024-08-04 19:53:55 +1200

    Realtime2
    Modify Realtime page(second time)

--- frontend/src/pages/realtime.jsx
+++ frontend/src/pages/realtime.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import NavBar from '../components/navBar.jsx';
-import Copyright from '../components/copyright.jsx';  
+import Copyright from '../components/copyright.jsx';
 import '../css/realtime.css';
 
 const Realtime = () => {
+
   return (
-    <div className='main_content'> 
+    <div className='realtime_background'>
         <NavBar/>
-          <div className='main_content'> 
-              <div className='main_container'>
-                <div className='main_box1'>
-                  <div className='page_bigbox1'>
-                    <div className='page_bigbox1_title'>실시간 인기알바
-                      <div className='page_bigbox1_subtitle'>실시간 인기순위, 인기알바
-                        </div>
-                      </div>
-                    </div>
+          <div className='realtime_content'>
+            <div className='realtime_container'>
+              <div className = 'realtime_box1'>
+                <div className='rbox1_content'>
+                  <div className='rbox1_title1'>실시간 인기알바
+                  <div className='rbox1_title2'>실시간 인기순위, 인기알바</div>
                   </div>
-                  <div className = 'realtime_subbox'>
-                      
-                        <div className='realtime_content'>
-                          <div className='box4_title'>카테고리별 <br/> 인기알바</div>
-                        </div>
-                        <div className='realtime_content'>
-                          <div className='realtime_subbox_title'>카테고리별 <br/> 인기알바</div>
-                        </div>
-                      
-                      </div>
-                    </div>
-                  <Copyright/>
                 </div>
               </div>
+              </div>
+              <div className='realtime_box2'>
+                <div className='rbox2_content_container'>
+                  <div className='rbox2_content'>
+                    <div className='rbox2_content_left'>
+                      <h1>카테고리</h1>
+                    </div>
+                    <div className='rbox2_content_right'></div>
+                  </div>
+                  <div className='rbox2_content'>
+                    <div className='rbox2_content_left'>
+                      <h1>실시간 인기알바 순위</h1>
+                    </div>
+                    <div className='rbox2_content_right'></div>
+                  </div>
+                </div>
+              </div>
+              <Copyright/>
+        </div>
+    </div>
   )
 }
 
