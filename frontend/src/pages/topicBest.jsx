2a2d70baaba4b150554a7cd86b549353b5c55a9c
JohnJung-1017 <jungih1017@gmail.com>
2024-08-21 05:53:06 +0900

    feat: topicBest
    카테고리 x축 scroll 생성 및 선택 기능 생성

--- frontend/src/pages/topicBest.jsx
+++ frontend/src/pages/topicBest.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/navBar.jsx";
 import Copyright from "../components/copyright.jsx";
 import PostList from "./PostList.jsx";
 import "../css/topicBest.css";
 
 const TopicBest = () => {
+  const [activeButtonIndex, setActiveButtonIndex] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setActiveButtonIndex(index);
+  };
+
   return (
     <div className="main_content">
       <NavBar />
       <div className="main_content">
         {/* <PostList/> */}
         <div className="tb_container">
-          <div className="tb_title">토픽베스트</div>
+          <div className="tb_title">
+            <h1 className="tb_title1">토픽 베스트</h1>
+            <h3 className="tb_title2">실시간 토픽 베스트, 인기 카테고리</h3>
+          </div>
           <div className="tb_box1">
-            <button className="tb_box1_button">토픽 베스트</button>
-            <button className="tb_box1_button">토픽 A</button>
-            <button className="tb_box1_button">토픽 B</button>
-            <button className="tb_box1_button">토픽 C</button>
-            <button className="tb_box1_button">토픽 D</button>
-            <button className="tb_box1_button">토픽 E</button>
-            <button className="tb_box1_button">토픽 F</button>
+            {[
+              "토픽 베스트",
+              "알바 썰",
+              "진상 모음",
+              "꿀알발 추천",
+              "패션/브랜드",
+              "중고거래",
+              "게임/엔터테이먼트",
+              "주식/코인",
+              "연애/상담",
+            ].map((topic, index) => (
+              <button
+                key={index}
+                className={`tb_box1_button ${
+                  activeButtonIndex === index ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick(index)}
+              >
+                {topic}
+              </button>
+            ))}
           </div>
           <div className="tb_box2">
             <div className="tb_box2_grid">
